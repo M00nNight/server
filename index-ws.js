da0713f1ad3fa15a1097a128143c7c9cde731a52
Zamil Khan <khanzamil267@gmail.com>
2024-01-04 16:32:10 +0530

    code change for better result

--- index-ws.js
+++ index-ws.js
@@ -1,43 +1,42 @@
-const express = require("express");
-const http = require('http');
-const WebSocket = require('ws');
+const express= require("express");
+const { devNull } = require("os");
+const server= require('http').createServer();
+const app  = express();
 
-const app = express();
-const server = http.createServer(app);
+app.get('/',function(req,res){
+    res.sendFile('index.html',{root:__dirname});
+})
 
-app.use(express.static(__dirname)); // Serve static files
+server.on('request',app);
+server.listen(4000,function(){console.log('server started on port 3000')})
 
-app.get('/', function(req, res) {
-    res.sendFile('index.html', { root: __dirname });
-});
 
-const wss = new WebSocket.Server({ server });
+//begin websocket
 
-wss.on('connection', function connection(ws) {
+const WebSocketServer = require('ws').Server;
+
+const wss= new WebSocketServer({server:server});
+
+wss.on('connection',function connection(ws){
     const numClients = wss.clients.size;
-    console.log('Clients connected:', numClients);
+    console.log('Clients connented',numClients);
 
-    wss.broadcast(`Current visitor: ${numClients}`);
+    wss.broadcast(`Current visitors: ${numClients}`);
 
-    if (ws.readyState === ws.OPEN) {
-        ws.send('Welcome to my server. Regards, Zamil Khan');
+    if(ws.readyState === ws.OPEN){
+        ws.send('Welcome to cyber chat, regards from Zamil :)');
+
+
     }
-
-    ws.on('close', function close() {
-        const numClients = wss.clients.size;
+    ws.on('close',function close(){
+        console.log('Client disconnected');
         wss.broadcast(`Current visitors: ${numClients}`);
-        console.log('A client has disconnected');
-    });
+    })
+
 });
 
-wss.broadcast = function broadcast(data) {
-    wss.clients.forEach(function each(client) {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(data);
-        }
-    });
-};
-
-server.listen(3000, function() {
-    console.log('Server started on port 3000');
-});
+wss.broadcast = function broadcast(data){
+    wss.clients.forEach(function each(client){
+        client.send(data);
+    })
+}
